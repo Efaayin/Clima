1d86b3ec2f5dd9eb8e74ab4a6a5c2e95a261c79c
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-11 23:14:01 +0100

    edits

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 Location location = Location();
 
@@ -17,10 +17,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   Future<void> getData() async {
-    Response response = await get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=137054c7985eadcbbfb130a6da542569'));
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=137054c7985eadcbbfb130a6da542569'));
 
     if (response.statusCode == 200) {
       String data = response.body;
+      print(data);
     } else {
       print(response.statusCode);
     }
