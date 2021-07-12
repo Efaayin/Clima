cc02a9ec89e69f6be876ba7ea20327b7d8381d5a
Ifeanyi <alichiifeanyi@yahoo.com>
2021-07-12 14:21:02 +0100

    edits

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,14 @@ const apiKey = '137054c7985eadcbbfb130a6da542569';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
 
